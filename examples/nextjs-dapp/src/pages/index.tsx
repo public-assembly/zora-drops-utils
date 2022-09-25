709dc294ba0fe63bbed1a133e072e2d2656537e3
Dain M. Blodorn Kim <dain@db13.us>
2022-09-25 19:25:21 -0400

    Remove boilerplate code from template repo (#14)
    * remove references to sample packages in react app, and small tsc error fixes
    
    * remove example packages from nextjs app
    
    * remove sample packages
    
    * remove example code, name package directory, update package.json

--- examples/nextjs-dapp/src/pages/index.tsx
+++ examples/nextjs-dapp/src/pages/index.tsx
@@ -1,14 +1,7 @@
-import { TestComponent } from 'react-package'
-import { useAccount } from 'wagmi'
-import { DynamicComponent } from '../components/example/DynamicComponent'
-
 function Page() {
-  const { address } = useAccount()
-
   return (
     <section className="flex flex-col gap-4">
-      <TestComponent />
-      {address ? <DynamicComponent address={address} /> : null}
+      <h1>Nextjs App</h1>
     </section>
   )
 }
