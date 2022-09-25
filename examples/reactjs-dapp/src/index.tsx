a4b724e228eccd17e7154f2fffc0f594777f9ba2
dblodorn <dain@db13.us>
2022-09-25 19:03:18 -0400

    remove references to sample packages in react app, and small tsc error fixes

--- examples/reactjs-dapp/src/index.tsx
+++ examples/reactjs-dapp/src/index.tsx
@@ -1,12 +1,10 @@
 import ReactDOM from 'react-dom/client'
-import React from 'react'
 import { App } from './App'
-import { ExampleImports } from './components/example/ExampleImports'
 
 const domContainer = document.getElementById('main')
 const root = ReactDOM.createRoot(domContainer)
 root.render(
   <App>
-    <ExampleImports />
+    <h1>Package here</h1>
   </App>
 )
