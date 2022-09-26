918cb136ce102f5c227885e127cdfa7d2162ab4a
Dain Blodorn Kim <dain@db13.us>
2022-09-26 16:29:29 -0700

    Add return data and corresponding typings

--- packages/zora-drops-utils/src/context/DropsContextProvider.tsx
+++ packages/zora-drops-utils/src/context/DropsContextProvider.tsx
@@ -1,20 +1,52 @@
 import { ReactNode } from 'react'
 import { createContext, useContext } from 'react'
+import { DropsRequestProps } from '../typings'
+import { useSWRDropsRequest } from '../hooks'
 
 export type DropsContextProps = {
   children?: ReactNode
 }
 
-export type DropsContextReturnTypes = {}
-
-const DropsContextContext = createContext<DropsContextReturnTypes>({})
-
-export function useDropsContextProvider() {
-  return useContext(DropsContextContext)
+export type DropsContextReturnTypes = {
+  contractAddress?: string
+  data?: any /* DAIN TODO - spec data return typings */
+  error?: any
+  isLoading?: boolean
+  isValidAddress?: boolean
 }
 
-export function DropsContextProvider({ children }: DropsContextProps) {
+const DropsContext = createContext<DropsContextReturnTypes>({
+  contractAddress: undefined,
+  data: null,
+  error: undefined,
+  isLoading: undefined,
+  isValidAddress: undefined,
+})
+
+export function useDropsContextProvider() {
+  return useContext(DropsContext)
+}
+
+export function DropsContextProvider({
+  children,
+  contractAddress,
+  networkId = '1',
+}: DropsContextProps & DropsRequestProps) {
+  const { data, error, isLoading, isValidAddress } = useSWRDropsRequest({
+    contractAddress: contractAddress,
+    networkId: networkId,
+  })
+
   return (
-    <DropsContextContext.Provider value={{}}>{children}</DropsContextContext.Provider>
+    <DropsContext.Provider
+      value={{
+        contractAddress,
+        data,
+        error,
+        isLoading,
+        isValidAddress,
+      }}>
+      {children}
+    </DropsContext.Provider>
   )
 }
