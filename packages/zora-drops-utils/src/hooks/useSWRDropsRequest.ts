f630e4b229f36f55058324046097801af33ea8d8
Dain M. Blodorn Kim <dain@db13.us>
2022-09-26 18:53:24 -0400

    adds address validation along with proper loading state in returns for both data queries (#20)

--- packages/zora-drops-utils/src/hooks/useSWRDropsRequest.ts
+++ packages/zora-drops-utils/src/hooks/useSWRDropsRequest.ts
@@ -1,21 +1,35 @@
 import useSWR from 'swr'
+import React from 'react'
 import { EDITION_QUERY, dropsFetcher } from '../data'
 import { DropsRequestProps } from '../typings'
+import { useValidAddress } from './useValidAddress'
 
 export function useSWRDropsRequest({
   contractAddress,
   networkId = '1',
 }: DropsRequestProps) {
-  const { data, error } = useSWR(
+  const [isLoading, setIsLoading] = React.useState(true)
+  const isValidAddress = useValidAddress(contractAddress)
+
+  const { data, error, isValidating } = useSWR(
     [networkId, contractAddress, EDITION_QUERY],
     dropsFetcher,
     {
+      isPaused: () => !isValidAddress,
+      onSuccess: () => setIsLoading(false),
+      onError: () => setIsLoading(false),
       errorRetryCount: 1,
     }
   )
 
+  React.useEffect(() => {
+    if (isValidating) setIsLoading(true)
+  }, [isValidating])
+
   return {
-    data: data,
-    error: error,
+    data,
+    error,
+    isLoading,
+    isValidAddress,
   }
 }
