37eed006b1b510c3228280da2cf9723ac82d9d2a
Dain Blodorn Kim <dain@db13.us>
2022-09-28 14:20:18 -0700

    drops array update

--- examples/reactjs-dapp/src/components/testing/DropsArray.tsx
+++ examples/reactjs-dapp/src/components/testing/DropsArray.tsx
@@ -2,7 +2,7 @@ import { useSWRDropsArray } from "@public-assembly/zora-drops-utils/src/hooks"
 import { RawDisplayer } from "../RawDisplayer"
 
 export function DropsArray({contracts = []}: {contracts: string[]} ) {
-  const { data } = useSWRDropsArray({ contractAddresses: contracts })
+  const { data } = useSWRDropsArray({ contractAddresses: contracts, refreshInterval: 0 })
   
   return (
     <div><RawDisplayer data={data} /></div>
