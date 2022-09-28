5478099b67bb639b9d73071f21cd8d581b646c9e
Dain Blodorn Kim <dain@db13.us>
2022-09-28 14:25:47 -0700

    add new functions to exports in package

--- packages/zora-drops-utils/src/index.ts
+++ packages/zora-drops-utils/src/index.ts
@@ -1,11 +1,21 @@
-export { EDITIONS_STYLE_CONTRACT_METADATA, EDITION_QUERY, dropsFetcher } from './data'
+export {
+  EDITIONS_STYLE_CONTRACT_METADATA,
+  EDITION_QUERY,
+  dropsFetcher,
+  dropsArrayFetcher,
+} from './data'
+
 export { DROPS_SUBGRAPH_URLS, returnDropEndpoint } from './constants'
-export { useDrop, useSWRDrop } from './hooks'
+
+export { useDrop, useSWRDrop, useDropsArray, useSWRDropsArray } from './hooks'
+
 export {
   DropContextProvider,
   useDropContextProvider,
   DropsContextProvider,
   useDropsContextProvider,
 } from './context'
+
 export { addIPFSGateway } from './lib/addIPFSGateway'
-export type { DropsRequestProps } from './typings'
+
+export type { DropsRequestProps, DropsArrayRequestProps } from './typings/requestTypes'
